f562d865a9836cbadb010f03e3db262d393a110d
Renat <theprojectstufftv@gmail.com>
2018-04-05 22:57:36 +0300

    commit clone

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main()
     int a,b;
     scanf("%d%d", &a, &b);
     printf("additon of a and b is qual - %d\n", a+b);
+    printf("difference of a and b is qual - %d\n", a-b);
     return 0;
 }
 
